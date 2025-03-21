b153b07955cab61cddbcebc6fc34ee900a39fa1b
rifqanzalbina <rifqiadiansyahrq@gmail.com>
2025-03-21 09:44:22 +0700

    updated

---
+++ product_generator/__init__.py
@@ -0,0 +1,12 @@
+"""
+texstat_id - Library untuk analisis statistik teks Bahasa Indonesia
+"""
+
+# Import fungsi-fungsi langsung ke namespace package
+from .generator import generate_product_name
+
+
+__version__ = '0.0.1'
+__all__ = [
+    'generate_product_name'
+]
